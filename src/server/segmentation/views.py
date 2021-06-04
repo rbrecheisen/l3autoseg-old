9571be548b5a4af0e5ecebc25a4f1a6e17ae7304
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-04 09:28:18 +0200

    Refactored segmentation code with Django RQ

--- src/server/segmentation/views.py
+++ src/server/segmentation/views.py
@@ -35,43 +35,30 @@ def datasets(request):
 @login_required(login_url='/segmentation/accounts/login/')
 def dataset(request, dataset_id):
     ds = DataSetModel.objects.get(pk=dataset_id)
-    action = request.GET.get('action', None)
-    if action == 'delete':
-        ds.delete()
-        dds = DataSetModel.objects.all()
-        return render(request, 'datasets.html', context={'datasets': dds})
     images = ImageModel.objects.filter(dataset=ds).all()
+    time_req = duration(8 * len(images))
+    action = request.GET.get('action', None)
+    if action == 'clear':
+        ds.job_id = None
+        ds.save()
+        for img in images:
+            img.job_status = None
+            img.save()
+        return render(request, 'dataset.html', context={'dataset': ds, 'images': images, 'time_req': time_req})
     q = django_rq.get_queue('default')
-    start_time = -1
     if action == 'segment':
-        start_time = current_time_secs()
-        print('starting segmentation...')
         job = q.enqueue(segment_images, images)
         ds.job_id = job.id
         ds.save()
         for img in images:
             img.job_status = 'queued'
             img.save()
-    time_req = duration(8 * len(images))
     if ds.job_id:
         job = q.fetch_job(ds.job_id)
         if job and job.get_status() == 'finished':
             for img in images:
-                img.job_status = 'finished'
-                img.png_file_name, img.png_file_path = create_png(img)
-                img.save()
-            if start_time < 0:
-                raise RuntimeError('Start time is -1')
-            elapsed = elapsed_secs(start_time)
-            print('segmentation ready after {}'.format(duration(elapsed)))
-    images = ImageModel.objects.filter(dataset=ds).all()
-    # for img in images:
-    #     if img.job_id:
-    #         job = q.fetch_job(img.job_id)
-    #         if job:
-    #             img.job_status = job.get_status()
-    #             if img.job_status == 'finished':
-    #                 img.pred_file_name, img.pred_file_path = job.result
-    #                 img.png_file_name, img.png_file_path = create_png(img)
-    #             img.save()
+                if img.png_file_name is None:
+                    img.png_file_name, img.png_file_path = create_png(img)
+                    img.save()
+    # TODO: check that image job status has been updated, i.e., should be "queued"
     return render(request, 'dataset.html', context={'dataset': ds, 'images': images, 'time_req': time_req})
