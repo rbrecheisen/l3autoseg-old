fb2a1df8a3a2654a6aeaa3ff45456afcc16961c4
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-16 12:56:35 +0200

    Small UI changes

--- src/l3autoseg/app/views.py
+++ src/l3autoseg/app/views.py
@@ -43,9 +43,6 @@ def dataset(request, dataset_id):
         return render(request, 'datasets.html', context={'datasets': dds})
     q = django_rq.get_queue('default')
     if action == 'segment':
-        # Get model version
-        print('Model version: {}'.format(request.GET.get('models', None)))
-        # Clear any previous results
         for img in images:
             img.clear_results()
         job = q.enqueue(segment_images, images)
