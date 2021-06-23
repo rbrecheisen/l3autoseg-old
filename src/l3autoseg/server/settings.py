2bb90b753f5bd020e23da607b366f5145bf1681f
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-23 11:44:55 +0200

    Added logout redirect

--- src/l3autoseg/server/settings.py
+++ src/l3autoseg/server/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
