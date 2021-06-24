ca9e06cdf4492f74f6d4b71c9d10165b90e8d0d6
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-24 13:15:40 +0200

    Updated DICOM file handling

--- src/l3autoseg/app/views.py
+++ src/l3autoseg/app/views.py
@@ -31,10 +31,7 @@ def datasets(request):
         errors = []
         for f in files:
             print('Checking DICOM type {}'.format(f))
-            try:
-                pydicom.dcmread(f, stop_before_pixels=True)
-                f.seek(0)
-            except pydicom.errors.InvalidDicomError:
+            if not is_dicom_file(f):
                 err = 'File {} is not a DICOM file'.format(f)
                 errors.append(err)
                 print(err)
@@ -43,7 +40,6 @@ def datasets(request):
             ds = DataSetModel.objects.create(name='dataset-{}'.format(timestamp), owner=request.user)
             for f in files:
                 p = pydicom.dcmread(f, stop_before_pixels=True)
-                f.seek(0)
                 if p.Rows != 512 or p.Columns != 512:
                     err = 'File {} has wrong dimensions ({} x {})'.format(f, p.Rows, p.Columns)
                     errors.append(err)
