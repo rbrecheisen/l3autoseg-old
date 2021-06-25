b7aa27dc3377c11e483d3fc31c15f234fed5f0f0
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-25 10:45:22 +0200

    Working on test for uploading images

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -18,4 +18,5 @@ def test_user_can_create_dataset_by_uploading_images():
         f_path = os.path.join(IMG_DIR, f)
         files.append((f, open(f_path, 'rb')))
     dataset_id = post_files('/datasets/', token, files=files, raw_result=True)
+    print(dataset_id)
     assert dataset_id is not None
