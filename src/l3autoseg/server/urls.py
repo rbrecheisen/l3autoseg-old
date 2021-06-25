65664918b0f582161dc7a8813fc0aa34c85dc667
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-25 09:57:25 +0200

    After failing test, added token requests end-point

--- src/l3autoseg/server/urls.py
+++ src/l3autoseg/server/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('token-requests/', obtain_auth_token, name='token-requests'),
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
