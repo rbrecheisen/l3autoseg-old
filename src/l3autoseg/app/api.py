21403602254b867e8bd7aa1fe409ddaa5fbca3be
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-25 11:23:12 +0200

    Working on test for uploading images

--- src/l3autoseg/app/api.py
+++ src/l3autoseg/app/api.py
@@ -14,7 +14,8 @@ def datasets(request):
             dataset_ids.append(obj.id)
         return Response({'dataset_ids': dataset_ids}, status=200)
     if request.method == 'POST':
-        file_objs = request.FILES
-        print(file_objs)
+        file_objs = request.FILES.get('files')
+        for file_obj in file_objs:
+            print(file_obj)
         return Response({'dataset_id': 0}, status=201)
     return Response({}, status=404)
