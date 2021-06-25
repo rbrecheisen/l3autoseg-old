00bd923d79efa68ff8e0eb46bede4a3c6701cbdb
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-25 10:38:02 +0200

    Created test for uploading images

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,6 +1,23 @@
-from .utils import get_token
+import os
+from .utils import get_token, post_files
+
+
+HOME = os.environ['HOME']
+IMG_DIR = '{}/data/l3autoseg/test_files'.format(HOME)
 
 
 def test_user_can_get_token():
     token = get_token('ralph', 'arturo4ever')
     print(token)
+
+
+def test_user_can_create_dataset_by_uploading_images():
+    token = get_token('ralph', 'arturo4ever')
+    files = []
+    for f in os.listdir(IMG_DIR):
+        f = os.path.join(IMG_DIR, f)
+        files.append(open(f, 'rb'))
+    dataset_id = post_files('/datasets/', token, files={'files': files})
+    assert dataset_id is not None
+    for f in files:
+        f.close()
