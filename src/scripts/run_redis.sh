9caf507e7c90ad981a441b67f1c111e81186acf7
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-30 11:21:38 +0200

    Saving work

--- src/scripts/run_redis.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -d -p 6378:6379 --name l3autoseg_redis redis redis-server
--- src/scripts/run_rq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2164
-cd l3autoseg
-# options: default high low --burst
-python manage.py rqworker default
